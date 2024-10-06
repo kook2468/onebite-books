0b22bdc400394325e332ac3264b10885970c5523
kook2468 <cdg9808@naver.com>
2024-10-07 01:40:17 +0900

    서버 도메인 수정

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -4,7 +4,7 @@ export default async function fetchOneBook(
   id: number
 ): Promise<BookData | null> {
   //BookData나 null값이 반환된다.
-  const url = `http://localhost:12345/book/${id}`;
+  const url = `https://onebite-books-server-nine-pi.vercel.app/book/${id}`;
 
   try {
     const response = await fetch(url);
